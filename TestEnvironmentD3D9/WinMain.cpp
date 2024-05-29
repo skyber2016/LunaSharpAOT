6900d0fb148babdb60c25047b9ea054b60deff8f
Nguyen Hoang Duy <nguyenhoangduy1997@gmail.com>
2024-05-29 03:01:04 -0700

    add dinput8

--- TestEnvironmentD3D9/WinMain.cpp
+++ TestEnvironmentD3D9/WinMain.cpp
@@ -1,16 +1,145 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <d3d9.h>
 #include <d3dx9.h>
 #include "NativeWindow.h"
-
+#include <InitGuid.h>
+#include <dinput.h>
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
-
+#pragma comment(lib, "dinput8")
 IDirect3D9* pD3d = nullptr;
 IDirect3DDevice9* pDevice = nullptr;
 ID3DXFont* pFont = nullptr;
 ID3DXSprite* pSprite = nullptr;
 char szDevice[256]{ 0 };
+
+
+// Khai báo biến toàn cục
+LPDIRECTINPUT8 g_pDI = NULL;
+LPDIRECTINPUTDEVICE8 g_pKeyboard = NULL;
+LPDIRECTINPUTDEVICE8 g_pMouse = NULL;
+
+
+
+// Khởi tạo DirectInput
+HRESULT InitDirectInput(HINSTANCE hInstance)
+{
+	// Tạo đối tượng DirectInput
+	if (FAILED(DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&g_pDI, NULL)))
+	{
+		return E_FAIL;
+	}
+
+	return S_OK;
+}
+
+HRESULT InitKeyboard(HWND hWnd)
+{
+	// Tạo thiết bị bàn phím
+	if (FAILED(g_pDI->CreateDevice(GUID_SysKeyboard, &g_pKeyboard, NULL)))
+	{
+		return E_FAIL;
+	}
+
+	// Thiết lập định dạng dữ liệu cho thiết bị
+	if (FAILED(g_pKeyboard->SetDataFormat(&c_dfDIKeyboard)))
+	{
+		return E_FAIL;
+	}
+
+	// Thiết lập cấp độ hợp tác
+	if (FAILED(g_pKeyboard->SetCooperativeLevel(hWnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE)))
+	{
+		return E_FAIL;
+	}
+
+	// Lấy quyền điều khiển thiết bị
+	g_pKeyboard->Acquire();
+
+	return S_OK;
+}
+
+HRESULT InitMouse(HWND hWnd)
+{
+	// Tạo thiết bị chuột
+	if (FAILED(g_pDI->CreateDevice(GUID_SysMouse, &g_pMouse, NULL)))
+	{
+		return E_FAIL;
+	}
+
+	// Thiết lập định dạng dữ liệu cho thiết bị
+	if (FAILED(g_pMouse->SetDataFormat(&c_dfDIMouse2)))
+	{
+		return E_FAIL;
+	}
+
+	// Thiết lập cấp độ hợp tác
+	if (FAILED(g_pMouse->SetCooperativeLevel(hWnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE)))
+	{
+		return E_FAIL;
+	}
+
+	// Lấy quyền điều khiển thiết bị
+	g_pMouse->Acquire();
+
+	return S_OK;
+}
+
+BYTE diKeys[256];
+
+void ReadKeyboard()
+{
+	HRESULT hr;
+	hr = g_pKeyboard->GetDeviceState(sizeof(diKeys), (LPVOID)&diKeys);
+	if (FAILED(hr))
+	{
+		// Nếu thiết bị bị mất hoặc không được lấy quyền, lấy lại quyền điều khiển
+		if ((hr == DIERR_INPUTLOST) || (hr == DIERR_NOTACQUIRED))
+		{
+			g_pKeyboard->Acquire();
+		}
+	}
+}
+
+DIMOUSESTATE2 mouseState;
+
+void ReadMouse()
+{
+	HRESULT hr;
+	hr = g_pMouse->GetDeviceState(sizeof(DIMOUSESTATE2), &mouseState);
+	if (FAILED(hr))
+	{
+		// Nếu thiết bị bị mất hoặc không được lấy quyền, lấy lại quyền điều khiển
+		if ((hr == DIERR_INPUTLOST) || (hr == DIERR_NOTACQUIRED))
+		{
+			g_pMouse->Acquire();
+		}
+	}
+}
+
+void CleanupDirectInput()
+{
+	if (g_pKeyboard)
+	{
+		g_pKeyboard->Unacquire();
+		g_pKeyboard->Release();
+		g_pKeyboard = NULL;
+	}
+
+	if (g_pMouse)
+	{
+		g_pMouse->Unacquire();
+		g_pMouse->Release();
+		g_pMouse = NULL;
+	}
+
+	if (g_pDI)
+	{
+		g_pDI->Release();
+		g_pDI = NULL;
+	}
+}
+
 
 bool InitD3D(HWND hWnd, UINT uWidth, UINT uHeight)
 {
@@ -68,10 +197,19 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 	NativeWindow wnd;
 
 	wnd.Create( hInstance, nCmdShow );
-
-	if (!InitD3D( wnd.GetHandle(), WND_WIDTH, WND_HEIGHT ))
+	auto hWnd = wnd.GetHandle();
+	if (!InitD3D(hWnd, WND_WIDTH, WND_HEIGHT ))
 		return 1;
+	// Khởi tạo Direct3D, DirectInput, và các thành phần khác
+	if (FAILED(InitDirectInput(hInstance)))
+	{
+		return 0;
+	}
 
+	if (FAILED(InitKeyboard(hWnd)) || FAILED(InitMouse(hWnd)))
+	{
+		return 0;
+	}
 	MSG m;
 	while (true)
 	{
@@ -84,7 +222,13 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 			break;
 
 		Render(wnd);
+		// Đọc trạng thái đầu vào
+		ReadKeyboard();
+		ReadMouse();
 	}
+
+	// Dọn dẹp tài nguyên
+	CleanupDirectInput();
 
 	return 0;
 }
